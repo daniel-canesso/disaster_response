324226c96c913dc3293a9fff4195e5db04bc718d
Daniel Bicalho <38555418+shaoleen00@users.noreply.github.com>
2021-05-27 06:36:30 -0300

    Add files via upload

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,70 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.pipeline import Pipeline
+import sklearn.metrics as m
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+import pickle
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
 
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///%s' % (database_filepath))
+    df = pd.read_sql_table('Message', engine)
+    X = df.loc[:,'message']
+    y = df.iloc[:,4:]
+    return X, y, y.columns
 
 
 def tokenize(text):
-    pass
+    stop_words = set(stopwords.words('english'))
+    text = text.lower()
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+    return tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(
+            n_jobs=-1, 
+            class_weight='balanced',
+            min_samples_split=4
+        )))
+    ])
+    return pipeline
+    
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    pred = model.predict(X_test)
+    pred = pd.DataFrame(pred, columns=category_names)
+    for column in pred:
+        print(column)
+        print(m.classification_report(Y_test[column], pred[column]))
+        print('*' * 30)
+    
 
 
 def save_model(model, model_filepath):
-    pass
+    with open(model_filepath, 'wb') as file:  
+        pickle.dump(model, file)
 
 
 def main():
